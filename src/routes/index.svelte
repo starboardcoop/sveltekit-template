ae2259e0f5aab922e63e75752f788b56fb34929a
Cristina Zalucky <cristina.zalucky@gmail.com>
2021-05-23 18:44:49 -0400

    Undo
    Undo commit from template that belongs to another repo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,5 @@
 <script>
     import Heading from "$lib/Heading.svelte";
-    import img from "/img/pieChart.png";
 </script>
 
 <Heading>Hello, World!</Heading>
-
-<div class="flex flex-row">
-    <img class="width:100%; height:auto" src={img} alt="background image" />
-</div>
