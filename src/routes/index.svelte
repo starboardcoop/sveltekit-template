ed4108d253febe39f0a8358fc4fdc137d538d5ae
Cristina Zalucky <cristina.zalucky@gmail.com>
2021-05-23 18:23:48 -0400

    Add pie chart of portfolio

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,10 @@
 <script>
     import Heading from "$lib/Heading.svelte";
+    import img from "/img/pieChart.png";
 </script>
 
 <Heading>Hello, World!</Heading>
+
+<div class="flex flex-row">
+    <img class="width:100%; height:auto" src={img} alt="background image" />
+</div>
